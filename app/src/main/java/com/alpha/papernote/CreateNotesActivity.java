938e92b6157c1992b63810fbe5ecd8b49f55760a
Rizqi Al-Faruq <alfaruqrizqi18@gmail.com>
2019-05-16 23:13:10 +0700

    add create note activity, add animation transition, add gesture swipe to open activity

---
+++ app/src/main/java/com/alpha/papernote/CreateNotesActivity.java
@@ -0,0 +1,81 @@
+package com.alpha.papernote;
+
+import android.os.Build;
+import android.support.design.widget.CoordinatorLayout;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.alpha.papernote.gesture_helper.OnSwipeTouchListener;
+
+public class CreateNotesActivity extends AppCompatActivity {
+    Toolbar toolbar;
+    CoordinatorLayout container;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            getWindow().getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_LIGHT_STATUS_BAR);
+        }
+        setContentView(R.layout.activity_create_notes);
+        container = findViewById(R.id.container);
+
+        toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setDisplayShowTitleEnabled(false);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setDisplayShowHomeEnabled(true);
+
+        starterPack();
+    }
+
+
+    void starterPack(){
+        containerGesture();
+    }
+
+    void containerGesture() {
+        container.setOnTouchListener(new OnSwipeTouchListener(CreateNotesActivity.this) {
+            public void onSwipeTop() {
+                Toast.makeText(CreateNotesActivity.this, "top", Toast.LENGTH_SHORT).show();
+            }
+            public void onSwipeRight() {
+                finish();
+            }
+            public void onSwipeLeft() {
+                Toast.makeText(CreateNotesActivity.this, "left", Toast.LENGTH_SHORT).show();
+            }
+            public void onSwipeBottom() {
+                Toast.makeText(CreateNotesActivity.this, "bottom", Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.create_notes_activity_toolbar_menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case android.R.id.home:
+                finish();
+                return true;
+            case R.id.save:
+                Toast.makeText(CreateNotesActivity.this, "Saved", Toast.LENGTH_SHORT).show();
+                return true;
+
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
+}
--- app/src/main/java/com/alpha/papernote/MainActivity.java
+++ app/src/main/java/com/alpha/papernote/MainActivity.java
@@ -1,20 +1,24 @@
 package com.alpha.papernote;
 
-import android.app.ActionBar;
+import android.content.Intent;
 import android.os.Build;
 import android.support.design.widget.AppBarLayout;
+import android.support.design.widget.CoordinatorLayout;
+import android.support.design.widget.FloatingActionButton;
+import android.support.v4.widget.NestedScrollView;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.View;
-import android.view.Window;
-import android.view.WindowManager;
-import android.widget.TextView;
+import android.widget.Toast;
+
+import com.alpha.papernote.gesture_helper.OnSwipeTouchListener;
 
 public class MainActivity extends AppCompatActivity {
     Toolbar toolbar;
-    TextView toolbarTitle;
+    FloatingActionButton createNotes;
+    NestedScrollView root_layout;
     public boolean isEmptyStates = true;
 
     @Override
@@ -24,12 +28,37 @@ public class MainActivity extends AppCompatActivity {
             getWindow().getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_LIGHT_STATUS_BAR);
         }
         setContentView(R.layout.activity_main);
+        root_layout = findViewById(R.id.root_layout);
 
         toolbar = findViewById(R.id.toolbar);
-        toolbarTitle = findViewById(R.id.toolbarTitle);
+        createNotes = findViewById(R.id.createNotes);
         setSupportActionBar(toolbar);
         getSupportActionBar().setDisplayShowTitleEnabled(false);
+
+        starterPack();
+    }
+
+    void starterPack(){
+        containerGesture();
         checkIfEmptyStates();
+        createNotesClicked();
+    }
+
+    void containerGesture() {
+        root_layout.setOnTouchListener(new OnSwipeTouchListener(MainActivity.this) {
+            public void onSwipeLeft() {
+                startActivity(new Intent(MainActivity.this, CreateNotesActivity.class));
+            }
+            public void onSwipeTop() {
+                Toast.makeText(MainActivity.this, "top", Toast.LENGTH_SHORT).show();
+            }
+            public void onSwipeRight() {
+                Toast.makeText(MainActivity.this, "right", Toast.LENGTH_SHORT).show();
+            }
+            public void onSwipeBottom() {
+                Toast.makeText(MainActivity.this, "bottom", Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 
     void checkIfEmptyStates() {
@@ -44,10 +73,19 @@ public class MainActivity extends AppCompatActivity {
         }
     }
 
+    void createNotesClicked(){
+        createNotes.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MainActivity.this, CreateNotesActivity.class));
+            }
+        });
+    }
+
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.toolbar_menu, menu);
+        getMenuInflater().inflate(R.menu.main_activity_toolbar_menu, menu);
         return true;
     }
 }

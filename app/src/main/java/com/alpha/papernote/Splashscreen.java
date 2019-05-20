98c439115d82ebc305b4bc1c7030472aa6f94366
Rizqi Al-Faruq <alfaruqrizqi18@gmail.com>
2019-05-20 23:07:14 +0700

    add splashscreen

---
+++ app/src/main/java/com/alpha/papernote/Splashscreen.java
@@ -0,0 +1,40 @@
+package com.alpha.papernote;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
+import android.view.WindowManager;
+
+public class Splashscreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        setContentView(R.layout.acitivity_splashscreen);
+        new Handler().postDelayed(new Runnable() {
+
+            /*
+             * Showing splash screen with a timer. This will be useful when you
+             * want to show case your app logo / company
+             */
+
+            @Override
+            public void run() {
+                // This method will be executed once the timer is over
+                // Start your app main activity
+                Intent i = new Intent(Splashscreen.this, MainActivity.class);
+                startActivity(i);
+
+                // close this activity
+                finish();
+            }
+        }, 1500);
+    }
+
+    @Override
+    public void onBackPressed() {
+
+    }
+}

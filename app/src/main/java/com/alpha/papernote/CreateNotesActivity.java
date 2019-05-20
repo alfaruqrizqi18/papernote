1b159fea2f3ffb51d36442ed12f306a639274bc5
Rizqi Al-Faruq <alfaruqrizqi18@gmail.com>
2019-05-20 22:23:52 +0700

    fix status bar

--- app/src/main/java/com/alpha/papernote/CreateNotesActivity.java
+++ app/src/main/java/com/alpha/papernote/CreateNotesActivity.java
@@ -25,9 +25,6 @@ public class CreateNotesActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
-            getWindow().getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_LIGHT_STATUS_BAR);
-        }
         setContentView(R.layout.activity_create_notes);
         container = findViewById(R.id.container);
 
--- app/src/main/java/com/alpha/papernote/MainActivity.java
+++ app/src/main/java/com/alpha/papernote/MainActivity.java
@@ -25,9 +25,6 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
-            getWindow().getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_LIGHT_STATUS_BAR);
-        }
         setContentView(R.layout.activity_main);
         root_layout = findViewById(R.id.root_layout);
 
